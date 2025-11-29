efe5ff26e8996c79d0f3402956c883b747c44405
KostAnton <antonaleksandrkust@gmail.com>
2025-11-29 18:18:33 +0200

    Cards fetched.

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 //Initializing before starting the card game
 const Scraper = require('./quizzlet');
 console.log('Starting scraper...');
-async function run() {
-  await Scraper.start();
-  let cards = require('./data.json');
+function run() {
+  return new Promise(async (resolve, reject) => {
+    // await Scraper.start();
+    let cards = require('./data.json');
 
-  const LAST_CARD = 0;
-  const LEARNING_WINDOW = 20;
-  const RANDOM = true;
-  const REVERSE = false;
+    const LAST_CARD = 0;
+    const LEARNING_WINDOW = 20;
+    const RANDOM = true;
+    const REVERSE = false;
+    const FLIP = false;
 
-  let newSet = cards.slice(LAST_CARD, LAST_CARD + LEARNING_WINDOW);
-  if (RANDOM) {
-    newSet = shuffle(newSet, 30);
-  }
-  if (REVERSE) {
-    newSet = newSet.reverse();
-  }
+    let newSet = cards.slice(LAST_CARD, LAST_CARD + LEARNING_WINDOW);
+    if (RANDOM) {
+      newSet = shuffle(newSet, 30);
+    }
+    if (REVERSE) {
+      newSet = newSet.reverse();
+    }
+    //CHANGE FOR RTL SUPPORT
+    let temp;
+    newSet = newSet.map(card => card.map(sentance => sentance.split('').reverse().join('')));
 
-  console.log(newSet);
+    return resolve(newSet);
+  })
+  
 }
 
 function shuffle(array, times) {
@@ -36,4 +43,6 @@ function shuffle(array, times) {
   return tempArr;
 }
 
-run();
+run().then(cards => {
+  
+});
