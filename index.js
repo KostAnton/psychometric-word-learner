8156137676ed7734a88df11841dde145c4d0b4b3
KostAnton <antonaleksandrkust@gmail.com>
2025-12-04 17:09:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const LAST_CARD = 0;
+const LEARNING_WINDOW = 20;
+const RANDOM = true;
+const REVERSE = false;
+const FLIP = false;
 //Initializing before starting the card game
 const Scraper = require('./quizzlet');
 console.log('Starting scraper...');
@@ -5,12 +10,6 @@ function run() {
   return new Promise(async (resolve, reject) => {
     // await Scraper.start();
     let cards = require('./data.json');
-
-    const LAST_CARD = 0;
-    const LEARNING_WINDOW = 20;
-    const RANDOM = true;
-    const REVERSE = false;
-    const FLIP = false;
 
     let newSet = cards.slice(LAST_CARD, LAST_CARD + LEARNING_WINDOW);
     if (RANDOM) {
